abf5ff568cad93ee388b17df0960774119e0509e
itsrohitrk.official@gmail.com <itsrohitrk.official@gmail.com>
2026-01-12 23:10:13 +0530

    add 8 file

---
+++ Casting/Widening.java
@@ -0,0 +1,75 @@
+package Casting;
+
+public class Widening {
+    public static void main(String[] args) {
+
+        // byte → short → int → long → float → double
+        byte b = 100;
+        System.out.println("byte value    : " + b);
+
+        short s = b;
+        System.out.println("short value   : " + s);
+
+        int i = b;
+        System.out.println("int value     : " + i);
+
+        long l = b;
+        System.out.println("long value    : " + l);
+
+        float f = b;
+        System.out.println("float value  : " + f);
+
+        double d = b;
+        System.out.println("double value : " + d);
+
+      
+    }
+}
+
+
+
+  // short → int → long → float → double
+
+// package Casting;
+
+// public class Widening {
+//     public static void main(String[] args) {
+
+//         short s = 50;
+//         System.out.println("short value   : " + s);
+
+//         int i = s;
+//         System.out.println("int value     : " + i);
+
+//         long l = s;
+//         System.out.println("long value    : " + l);
+
+//         float f = s;
+//         System.out.println("float value   : " + f);
+
+//         double d = s;
+//         System.out.println("double value  : " + d);
+//     }
+// }
+
+
+  //int → long → float → double
+
+// package Casting;
+
+// public class Widening {
+//     public static void main(String[] args) {
+
+//         int i = 1000;
+//         System.out.println("int value     : " + i);
+
+//         long l = i;
+//         System.out.println("long value    : " + l);
+
+//         float f = i;
+//         System.out.println("float value   : " + f);
+
+//         double d = i;
+//         System.out.println("double value  : " + d);
+//     }
+// }
