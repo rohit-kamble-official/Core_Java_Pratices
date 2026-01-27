120f9b45969fb3671f34456c819a512a0b68d10f
itsrohitrk.official@gmail.com <itsrohitrk.official@gmail.com>
2026-01-28 01:21:34 +0530

    add 19 file

---
+++ Operator/AreaPerimeter.java
@@ -0,0 +1,17 @@
+package Operator;
+
+ class AreaPerimeter {
+    public static void main(String[] args) {
+
+        double width = 4.5;
+        double height = 7.9;
+
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        System.out.println("Width : " + width);
+        System.out.println("Height : " + height);
+        System.out.println("Area of rectangle : " + area);
+        System.out.println("Perimeter of rectangle : " + perimeter);
+    }
+}
