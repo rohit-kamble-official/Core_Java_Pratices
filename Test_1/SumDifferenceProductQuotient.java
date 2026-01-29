66f60a2473591cf7eca8970145914c7c6c2ba47c
itsrohitrk.official@gmail.com <itsrohitrk.official@gmail.com>
2026-01-29 15:42:09 +0530

    add 22 file

---
+++ Test_1/SumDifferenceProductQuotient.java
@@ -0,0 +1,17 @@
+package Test_1;
+
+public class SumDifferenceProductQuotient {
+    public static void main(String[] args) {
+        System.out.println("To calculte the sum , product , difference and quotient");
+        int a = 5;
+        int b = 4;
+        int sum = a+b;
+        int Product = a*b;
+        int sub = a-b;
+        int Divsion = a/b;
+        int Remeder  = a%b;
+        
+        System.out.println("sum : "+ sum + " Product : "+Product + " sub : "+sub + " Divison : "+ Divsion + " Remeder : " + Remeder);
+
+    }
+}
