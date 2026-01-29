5c4dc883bc5cddcd64f7f4b1237cee0566b9f484
itsrohitrk.official@gmail.com <itsrohitrk.official@gmail.com>
2026-01-29 16:59:15 +0530

    add 24 file

---
+++ Test_1/GreaterOrNot.java
@@ -0,0 +1,13 @@
+package Test_1;
+
+public class GreaterOrNot {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 20;
+
+        boolean isGreater = a >= b;
+
+        System.out.println("Is a greater than or equal to b? : " + isGreater);
+    }
+}
