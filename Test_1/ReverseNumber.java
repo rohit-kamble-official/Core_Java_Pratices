81064467d044075e9a889fc1e096e5d490b0b887
itsrohitrk.official@gmail.com <itsrohitrk.official@gmail.com>
2026-02-17 12:23:40 +0530

    add 29 file

---
+++ Test_1/ReverseNumber.java
@@ -0,0 +1,18 @@
+package Test_1;
+import java.util.Scanner;
+
+public class ReverseNumber {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a values : ");
+        int num = sc.nextInt();
+        int reverse = 0;
+
+        while (num>0) {
+            int digit = num%10;
+            reverse = reverse * 10 + digit;
+            num = num / 10;
+        }
+        System.out.println("reverse No : " + reverse);
+    }
+}
